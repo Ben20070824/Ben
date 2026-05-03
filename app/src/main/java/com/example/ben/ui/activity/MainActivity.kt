903e75ead473cded74cd9165809833557f3088be
Ben <2175801434@qq.com>
2026-05-03 11:04:39 +0800

    另一个activity框架搭好，准备实现账号管理

---
+++ app/src/main/java/com/example/ben/ui/activity/MainActivity.kt
@@ -0,0 +1,66 @@
+package com.example.ben.ui.activity
+
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.get
+import androidx.fragment.app.Fragment
+import androidx.viewpager2.widget.ViewPager2
+import com.example.ben.R
+import com.example.ben.adapter.ViewPagerAdapter
+import com.example.ben.databinding.ActivityMainBinding
+import com.example.ben.ui.fragment.AgentFragment
+import com.example.ben.ui.fragment.ChatFragment
+import com.example.ben.ui.fragment.ProfileFragment
+
+class MainActivity : AppCompatActivity() {
+    private val binding: ActivityMainBinding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        initView()
+    }
+
+    private fun initView() {
+        val fragments = listOf({ChatFragment()}, {AgentFragment()}, {ProfileFragment()})
+        binding.viewPager.apply {
+            adapter= ViewPagerAdapter(this@MainActivity,fragments)
+            offscreenPageLimit=2
+            registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback(){
+                override fun onPageSelected(position: Int) {
+                    super.onPageSelected(position)
+                    when(position){
+                        0-> binding.bottom.selectedItemId=R.id.navigation_chat
+                        1-> binding.bottom.selectedItemId=R.id.navigation_agents
+                        2-> binding.bottom.selectedItemId=R.id.navigation_profile
+                    }
+                }
+            })
+        }
+        binding.bottom.setOnItemSelectedListener { item ->
+            when(item.itemId){
+                R.id.navigation_chat -> binding.viewPager.currentItem=0
+                R.id.navigation_agents -> binding.viewPager.currentItem=1
+                R.id.navigation_profile -> binding.viewPager.currentItem=2
+                else -> throw Exception("bottomNavigation未知ID错误")
+            }
+            true
+        }
+    }
+    companion object{
+        fun start(context: Context){
+            val intent = Intent(context, MainActivity::class.java)
+            context.startActivity(intent)
+        }
+    }
+}
